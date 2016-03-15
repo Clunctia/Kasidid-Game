2ae53a56728e6f1fada61bb9e7c60d21131ff548
Goofy <goofy_clunctia@hotmail.com>
2016-03-15 11:07:57 +0900

    create git

---
+++ src/Character.js
@@ -0,0 +1,6 @@
+var Character = cc.Sprite.extend({
+   ctor: function(){
+       this._super();
+       this.initWithFile('res/images/Character.png');
+   } 
+});
---
+++ src/GameLayer.js
@@ -0,0 +1,23 @@
+var GameLayer = cc.LayerColor.extend({
+  init: function() {
+    this._super( new cc.Color( 127, 127, 127, 255 ) );
+    this.setPosition( new cc.Point( 0, 0 ) );
+    var char = new Character();
+    char.setPosition(new cc.Point(200,200));
+    this.addChild(char);
+    var enemy = new Enemy();
+    enemy.setPosition(new cc.Point(400,200));
+    this.addChild(enemy);
+    
+    return true;
+  }
+});
+
+var StartScene = cc.Scene.extend({
+  onEnter: function() {
+    this._super();
+    var layer = new GameLayer();
+    layer.init();
+    this.addChild( layer );
+  }
+});
